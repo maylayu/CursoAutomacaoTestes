e3e34cbf7644cb7bc58063c84b942ac3f7fcb7ef
maylayu <mayla.amaral@fatec.sp.gov.br>
2022-05-25 00:55:48 -0300

    -

--- target/reports/report.js
+++ target/reports/report.js
@@ -44,28 +44,28 @@ formatter.match({
   "location": "DescontosStep.acessar_site_qazando()"
 });
 formatter.result({
-  "duration": 5128767000,
+  "duration": 4091036000,
   "status": "passed"
 });
 formatter.match({
   "location": "DescontosStep.eu_preencho_meu_email()"
 });
 formatter.result({
-  "duration": 2196284200,
+  "duration": 2276902700,
   "status": "passed"
 });
 formatter.match({
   "location": "DescontosStep.clico_em_ganhar_cupom()"
 });
 formatter.result({
-  "duration": 84525400,
+  "duration": 110372300,
   "status": "passed"
 });
 formatter.match({
   "location": "DescontosStep.eu_vejo_o_código_de_desconto()"
 });
 formatter.result({
-  "duration": 49866400,
+  "duration": 48260700,
   "status": "passed"
 });
 formatter.scenario({
@@ -85,7 +85,8 @@ formatter.match({
   "location": "DescontosStep.gerarCupom()"
 });
 formatter.result({
-  "duration": 3054424400,
-  "status": "passed"
+  "duration": 15004666200,
+  "error_message": "org.openqa.selenium.WebDriverException: chrome not reachable\n  (Session info: chrome\u003d101.0.4951.64)\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027NOTEYUU\u0027, ip: \u0027192.168.1.88\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_311\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 101.0.4951.64, chrome: {chromedriverVersion: 100.0.4896.60 (6a5d10861ce8..., userDataDir: C:\\Users\\yukii\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:52391}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: 7e5a6315b519ab8716128c99f7edcd0d\n*** Element info: {Using\u003did, value\u003demail}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementById(RemoteWebDriver.java:372)\r\n\tat org.openqa.selenium.By$ById.findElement(By.java:188)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat pages.HomePage.preencheEmail(HomePage.java:32)\r\n\tat steps.DescontosStep.gerarCupom(DescontosStep.java:50)\r\n\tat ✽.Dado que tenha cupom gerado(desconto.feature:15)\r\n",
+  "status": "failed"
 });
 });
